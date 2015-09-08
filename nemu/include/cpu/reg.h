9bfaeafa68827ede3204a89e92517ab667799332
tracer-ics2015 <tracer@njuics.org>
2015-09-08 18:34:26 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:34:26 up 6 min,  2 users,  load average: 0.13, 0.13, 0.07 726994bb2d86e88dc03378b73ca15de9c264ef5

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -24,7 +24,7 @@ typedef union {
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+	struct {uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;};
 
 	
 	swaddr_t eip;

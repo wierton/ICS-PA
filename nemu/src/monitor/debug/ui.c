af378ac233d14158b2670578742f1ba3ff9ae731
tracer-ics2015 <tracer@njuics.org>
2015-09-09 21:21:31 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:21:31 up 37 min,  2 users,  load average: 0.00, 0.01, 0.05 f545d6a91dc9ab9b98af7fc4f5b823abadef298

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -132,7 +132,7 @@ static int cmd_x(char *args)
 	else if(*para >= '0' && *para <= '9')
 	{
 		sscanf(para,"%u%c%02x",&len,&xar,&addr);
-		printf("%u :%c: %02x",len,xar,addr);
+		printf("%u :%c: %x",len,xar,addr);
 		if(xar == 'x' || xar == 'X')
 		{}
 		else

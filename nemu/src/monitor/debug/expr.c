3280b4c61e3a35d63d760d89029881aa7e914a4b
tracer-ics2015 <tracer@njuics.org>
2015-09-17 16:30:04 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:30:04 up 33 min,  2 users,  load average: 0.17, 0.10, 0.07 abc24eb8a356ed797d956be06d6d71b859b4d0b6

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -263,7 +263,7 @@ uint32_t eval(char *e, bool *success) {
 	int i = 0;
 	while(is_valid && (Stack[pStack-1]._operator != '#' || unit[i]._operator != '#'))
 	{
-		//show(LinearTable,pLinearTable);
+		showlink(LinearTable,pLinearTable);
 		//show(Stack,pStack);
 		if(unit[i]._operator != 0)
 		{

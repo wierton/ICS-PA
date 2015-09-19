2f21fcec6a37cceb11c71da0e1f41b40c0b24466
tracer-ics2015 <tracer@njuics.org>
2015-09-19 22:54:15 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:54:15 up  3:38,  2 users,  load average: 0.03, 0.02, 0.05 444b4ff1f61806a9d5f88f1742e593780646647

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -68,6 +68,7 @@ static struct rule {
 	 * Pay attention to the precedence level of different rules.
 	 */
 
+	{"[+-]+[0-9]+", NUM},                       //number
 	{" +", NOTYPE},				// spaces
 	{"\t+", NOTYPE},
 	{"\\++", '+'},					// plus
@@ -78,7 +79,6 @@ static struct rule {
 	{"\\(", '('},						//left parenthese
 	{"\\)", ')'},						//right parethese
 	{"0[xX][0-9a-fA-F]+", HEX},				//hex
-	{"[+-]+[0-9]+", NUM},						//number
 	{"%[a-zA-Z]{3}", REG},
 	{"==", EQ}						// equal
 };

b422e2616b6ff0471e4482c84a51db566bda6dc4
tracer-ics2015 <tracer@njuics.org>
2015-10-03 23:20:38 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:20:38 up 11:00,  2 users,  load average: 0.01, 0.02, 0.05 6d1cba6b7771862584945f9a221b13922efe43a

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -11,9 +11,10 @@ make_instr_helper(i2a)
 make_instr_helper(i2rm)
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
-/*make_helper(concat(cmp_si2rm, SUFFIX) {
+
+make_helper(concat(cmp_si2rm_, SUFFIX)) {
 
 	return 5;
 }
-*/
+
 #include "cpu/exec/template-end.h"

496b0885c58f84f781b5541ac6b704cc7f9fcf48
tracer-ics2015 <tracer@njuics.org>
2015-10-04 19:28:51 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:28:51 up  9:19,  2 users,  load average: 0.00, 0.01, 0.05 cce507b154f7c7634ff9d31f78504e0476ce0757

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -4,10 +4,11 @@
 #define instr cmp
 
 static void do_execute() {
+	DATA_TYPE dst = op_dest->val - op_src->val;
 	cpu.OF = subOK(op_src->val, op_dest->val);
-	cpu.SF = (op_dest->val)>>31;
-	cpu.ZF = (op_dest->val == 0);
-	cpu.PF = anyEvenBit(op_dest->val);
+	cpu.SF = (dst)>>31;
+	cpu.ZF = (dst == 0);
+	cpu.PF = anyEvenBit(dst);
 	cpu.CF = (op_src->val < op_dest->val);
 	print_asm_template2();
 }

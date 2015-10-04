9ee986409abd4816fb9f9ef35859609eba2fa48b
tracer-ics2015 <tracer@njuics.org>
2015-10-04 18:49:09 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  18:49:09 up  8:39,  2 users,  load average: 0.05, 0.03, 0.05 64016562e7fd18f822713c4497a10b793c75921a

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -4,11 +4,7 @@
 #define instr sub
 
 static void do_execute() {
-	printf("%x,%x\n",op_dest->type == OP_TYPE_REG,op_dest->reg);
-	printf("esp:%x\t",cpu.esp);
-	printf("%x,%x\t", op_src->val, op_dest->val);
 	OPERAND_W(op_dest, op_src->val-op_dest->val);
-	printf("esp:%x\n",cpu.esp);
 	cpu.OF = subOK(op_src->val, op_dest->val);
 	cpu.SF = (op_dest->val)>>31;
 	cpu.ZF = (op_dest->val == 0);

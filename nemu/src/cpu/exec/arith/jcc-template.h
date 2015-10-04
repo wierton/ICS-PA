4448a570c89986a3fe999dde080494e09916b7de
tracer-ics2015 <tracer@njuics.org>
2015-10-04 16:08:30 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:08:30 up  5:58,  2 users,  load average: 0.18, 0.06, 0.06 7131d1924b8668e97c903bb638b31bc3ea0fccfc

--- nemu/src/cpu/exec/arith/jcc-template.h
+++ nemu/src/cpu/exec/arith/jcc-template.h
@@ -3,7 +3,6 @@
 #define instr jcc
 
 static void do_execute() {
-	//printf("arith/jcc-template.h:%x,%x\n", op_dest->val, op_src->val);
 	if(cpu.ZF == 1)
 		cpu.eip = cpu.eip + op_dest->val;
 	print_asm_template1();

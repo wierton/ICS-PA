39fa4bafb32941a3f958e81a0447e2f36e4ad369
tracer-ics2015 <tracer@njuics.org>
2015-10-04 12:18:53 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:18:53 up  2:09,  2 users,  load average: 0.10, 0.06, 0.05 e0bcb6726764ddfc8831b9bb23dfccead5c5955e

---
+++ nemu/src/cpu/exec/arith/test-template.h
@@ -0,0 +1,20 @@
+#include "cpu/exec/template-start.h"
+#include "arith-bits.h"
+
+#define instr test
+
+static void do_execute() {
+	DATA_TYPE dst = op_src->val & op_dest->val;
+	cpu.OF = 0;
+	cpu.SF = dst>>31;
+	cpu.ZF = (dst == 0);
+	cpu.PF = anyEvenBit(op_dest->val);
+	cpu.CF = 0;
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+
+
+#include "cpu/exec/template-end.h"

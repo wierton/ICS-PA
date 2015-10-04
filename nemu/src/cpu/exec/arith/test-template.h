0917169d2ea65056bb443875725cbd84dd8d7af9
tracer-ics2015 <tracer@njuics.org>
2015-10-04 19:23:29 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:23:29 up  9:13,  2 users,  load average: 0.09, 0.04, 0.05 492db35b8a785ad6f0e310d78b9ae53e9fbbe0ea

--- nemu/src/cpu/exec/arith/test-template.h
+++ nemu/src/cpu/exec/arith/test-template.h
@@ -4,13 +4,13 @@
 #define instr test
 
 static void do_execute() {
-	printf("TEST:%x,%x\n",op_src->val,op_dest->val);
 	DATA_TYPE dst = op_src->val & op_dest->val;
 	cpu.OF = 0;
 	cpu.SF = dst>>31;
 	cpu.ZF = (dst == 0);
 	cpu.PF = anyEvenBit(op_dest->val);
 	cpu.CF = 0;
+	printf("%x\n",cpu.ZF);
 	print_asm_template2();
 }
 

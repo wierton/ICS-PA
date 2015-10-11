cd92df2e7220af064c323c19a1db3a8e788ad08f
tracer-ics2015 <tracer@njuics.org>
2015-10-12 00:08:58 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:08:58 up  4:04,  2 users,  load average: 0.00, 0.01, 0.05 55933b317af70ffa60ce248eb9a6cf1aeb923f46

--- nemu/src/cpu/exec/arith/call-template.h
+++ nemu/src/cpu/exec/arith/call-template.h
@@ -6,7 +6,7 @@ static void do_execute() {
 	cpu.esp -= DATA_BYTE;
 	MEM_W(cpu.esp, cpu.eip);
 	cpu.eip += op_src->val;
-	print_asm("call %08x",cpu.eip);
+	print_asm("call 0x%08x",cpu.eip);
 }
 
 make_instr_helper(i)

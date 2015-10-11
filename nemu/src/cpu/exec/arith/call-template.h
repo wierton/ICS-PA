394583ada1c103d2ccf8e3f32610aee1a642ced9
tracer-ics2015 <tracer@njuics.org>
2015-10-12 00:37:59 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:37:59 up  4:33,  2 users,  load average: 0.11, 0.06, 0.05 611760c0c116a819662fbf4131c1590531c23dac

--- nemu/src/cpu/exec/arith/call-template.h
+++ nemu/src/cpu/exec/arith/call-template.h
@@ -6,7 +6,7 @@ make_helper(concat(call_i_, SUFFIX))
 {
 	int len = concat(decode_i_, SUFFIX)(cpu.eip+1);
 	cpu.esp -= DATA_BYTE;
-	MEM_W(cpu.esp, cpu.eip+len+1);
+	MEM_W(cpu.esp, cpu.eip+len);
 	cpu.eip += op_src->val;
 	print_asm("call 0x%8x",cpu.eip+len+1);
 	return len+1;

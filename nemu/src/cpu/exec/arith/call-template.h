d3800cf10bcc968b072b686a1415b28c6d3dd7dc
tracer-ics2015 <tracer@njuics.org>
2015-10-12 00:36:30 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:36:30 up  4:32,  2 users,  load average: 0.12, 0.05, 0.05 e12b5e61ff12798caa6be878e974a6d9c534eeb9

--- nemu/src/cpu/exec/arith/call-template.h
+++ nemu/src/cpu/exec/arith/call-template.h
@@ -2,14 +2,15 @@
 
 #define instr call
 
-static void do_execute() {
+make_helper(concat(call_i_, SUFFIX))
+{
+	int len = concat(decode_i_, SUFFIX)(cpu.eip+1);
 	cpu.esp -= DATA_BYTE;
-	MEM_W(cpu.esp, cpu.eip);
+	MEM_W(cpu.esp, cpu.eip+len+1);
 	cpu.eip += op_src->val;
-	print_asm("call 0x%08x",cpu.eip);
+	print_asm("call 0x%8x",cpu.eip+len+1);
+	return len+1;
 }
-
-make_instr_helper(i)
 
 
 #include "cpu/exec/template-end.h"

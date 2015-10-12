1adca012c4532b69a87b0c79236c2f614db12f3a
tracer-ics2015 <tracer@njuics.org>
2015-10-12 23:24:06 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:24:06 up  6:35,  2 users,  load average: 0.03, 0.04, 0.05 8f3372754a4da2b5db5ba6addb220cf664747ae1

---
+++ nemu/src/cpu/exec/data-mov/set-template.h
@@ -0,0 +1,139 @@
+#include "cpu/exec/template-start.h"
+
+#define instr set
+
+static void do_eflags_execute(bool flag,int diplacement,char set_flagstr[])
+{
+	DATA_TYPE_S eip_dis = op_src->val;
+	int eip_pos = cpu.eip;
+	if(flag)
+	{
+		cpu.eip = cpu.eip + eip_dis;
+	}
+	print_asm("%s $0x%x",set_flagstr,eip_pos+eip_dis+diplacement);
+}
+
+
+#define flagstr seta
+make_eflags_helper(i,!cpu.CF,!cpu.ZF)
+#undef flagstr
+
+#define flagstr setae
+make_eflags_helper(i,!cpu.CF,1)
+#undef flagstr
+
+#define flagstr setb
+make_eflags_helper(i,cpu.CF,1)
+#undef flagstr
+
+#define flagstr setbe
+make_eflags_helper(i,cpu.CF || cpu.ZF,1)
+#undef flagstr
+
+#define flagstr setc
+make_eflags_helper(i,cpu.CF,1)
+#undef flagstr
+
+#define flagstr sete
+make_eflags_helper(i,cpu.ZF,1)
+#undef flagstr
+
+#define flagstr setz
+make_eflags_helper(i,cpu.ZF,1)
+#undef flagstr
+
+#define flagstr setg
+make_eflags_helper(i,!cpu.ZF,cpu.SF==cpu.OF)
+#undef flagstr
+
+#define flagstr setge
+make_eflags_helper(i,cpu.SF==cpu.OF,1)
+#undef flagstr
+
+#define flagstr setl
+make_eflags_helper(i,cpu.SF!=cpu.OF,1)
+#undef flagstr
+
+#define flagstr setle
+make_eflags_helper(i,(cpu.ZF || (cpu.SF!=cpu.OF)),1)
+#undef flagstr
+
+#define flagstr setna
+make_eflags_helper(i,cpu.CF,cpu.SF)
+#undef flagstr
+
+#define flagstr setnae
+make_eflags_helper(i,cpu.CF,1)
+#undef flagstr
+
+#define flagstr setnb
+make_eflags_helper(i,!cpu.CF,1)
+#undef flagstr
+
+#define flagstr setnbe
+make_eflags_helper(i,!cpu.ZF,!cpu.ZF)
+#undef flagstr
+
+#define flagstr setnc
+make_eflags_helper(i,!cpu.CF,1)
+#undef flagstr
+
+#define flagstr setne
+make_eflags_helper(i,!cpu.ZF,1)
+#undef flagstr
+
+#define flagstr setng
+make_eflags_helper(i,cpu.ZF || (cpu.SF!=cpu.OF),1)
+#undef flagstr
+
+#define flagstr setnge
+make_eflags_helper(i,cpu.SF!=cpu.OF,1)
+#undef flagstr
+
+#define flagstr setnl
+make_eflags_helper(i,cpu.SF==cpu.OF,1)
+#undef flagstr
+
+#define flagstr setnle
+make_eflags_helper(i,!cpu.ZF,cpu.SF==cpu.OF)
+#undef flagstr
+
+#define flagstr setno
+make_eflags_helper(i,!cpu.OF,1)
+#undef flagstr
+
+#define flagstr setnp
+make_eflags_helper(i,!cpu.PF,1)
+#undef flagstr
+
+#define flagstr setns
+make_eflags_helper(i,!cpu.SF,1)
+#undef flagstr
+
+#define flagstr setnz
+make_eflags_helper(i,!cpu.ZF,1)
+#undef flagstr
+
+#define flagstr seto
+make_eflags_helper(i,cpu.OF,1)
+#undef flagstr
+
+#define flagstr setp
+make_eflags_helper(i,cpu.PF,1)
+#undef flagstr
+
+#define flagstr setpe
+make_eflags_helper(i,cpu.PF,1)
+#undef flagstr
+
+#define flagstr setpo
+make_eflags_helper(i,!cpu.PF,1)
+#undef flagstr
+
+#define flagstr sets
+make_eflags_helper(i,cpu.SF,1)
+#undef flagstr
+
+
+#include "cpu/exec/template-end.h"
+	
---
+++ nemu/src/cpu/exec/data-mov/set.c
@@ -0,0 +1,48 @@
+#include "cpu/exec/helper.h"
+
+#define DATA_BYTE 1
+#include "set-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 2
+#include "set-template.h"
+#undef DATA_BYTE
+
+#define DATA_BYTE 4
+#include "set-template.h"
+#undef DATA_BYTE
+
+/* for instruction encoding overloading */
+
+make_helper_v(seta_i)
+make_helper_v(setae_i)
+make_helper_v(setb_i)
+make_helper_v(setbe_i)
+make_helper_v(setc_i)
+make_helper_v(sete_i)
+make_helper_v(setz_i)
+make_helper_v(setg_i)
+make_helper_v(setge_i)
+make_helper_v(setl_i)
+make_helper_v(setle_i)
+make_helper_v(setna_i)
+make_helper_v(setnae_i)
+make_helper_v(setnb_i)
+make_helper_v(setnbe_i)
+make_helper_v(setnc_i)
+make_helper_v(setne_i)
+make_helper_v(setng_i)
+make_helper_v(setnge_i)
+make_helper_v(setnl_i)
+make_helper_v(setnle_i)
+make_helper_v(setno_i)
+make_helper_v(setnp_i)
+make_helper_v(setns_i)
+make_helper_v(setnz_i)
+make_helper_v(seto_i)
+make_helper_v(setp_i)
+make_helper_v(setpe_i)
+make_helper_v(setpo_i)
+make_helper_v(sets_i)
+
+

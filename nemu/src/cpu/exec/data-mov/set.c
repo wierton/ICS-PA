81da25187298bee8beaf128581444583b6978c5d
tracer-ics2015 <tracer@njuics.org>
2015-10-12 23:26:52 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:26:52 up  6:38,  2 users,  load average: 0.06, 0.05, 0.05 e01238d64ac881bbc7779bf54f195cea51dc604

--- nemu/src/cpu/exec/data-mov/set.c
+++ nemu/src/cpu/exec/data-mov/set.c
@@ -4,45 +4,5 @@
 #include "set-template.h"
 #undef DATA_BYTE
 
-#define DATA_BYTE 2
-#include "set-template.h"
-#undef DATA_BYTE
-
-#define DATA_BYTE 4
-#include "set-template.h"
-#undef DATA_BYTE
-
 /* for instruction encoding overloading */
-
-make_helper_v(seta_rm)
-make_helper_v(setae_rm)
-make_helper_v(setb_rm)
-make_helper_v(setbe_rm)
-make_helper_v(setc_rm)
-make_helper_v(sete_rm)
-make_helper_v(setz_rm)
-make_helper_v(setg_rm)
-make_helper_v(setge_rm)
-make_helper_v(setl_rm)
-make_helper_v(setle_rm)
-make_helper_v(setna_rm)
-make_helper_v(setnae_rm)
-make_helper_v(setnb_rm)
-make_helper_v(setnbe_rm)
-make_helper_v(setnc_rm)
-make_helper_v(setne_rm)
-make_helper_v(setng_rm)
-make_helper_v(setnge_rm)
-make_helper_v(setnl_rm)
-make_helper_v(setnle_rm)
-make_helper_v(setno_rm)
-make_helper_v(setnp_rm)
-make_helper_v(setns_rm)
-make_helper_v(setnz_rm)
-make_helper_v(seto_rm)
-make_helper_v(setp_rm)
-make_helper_v(setpe_rm)
-make_helper_v(setpo_rm)
-make_helper_v(sets_rm)
-
 

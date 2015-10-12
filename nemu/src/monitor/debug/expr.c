3dd2b53aa9ed3f9533195030e7f1e8bec9903990
tracer-ics2015 <tracer@njuics.org>
2015-10-12 21:34:31 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:34:31 up  4:46,  2 users,  load average: 0.01, 0.05, 0.05 6ea140e193958055e173ef7e6c62721817519de5

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -205,7 +205,7 @@ static bool make_token(char *e) {
 				int substr_len = pmatch.rm_eo;
 
 				assert(pUnit >= 0 && pUnit <= UnitNum);
-				Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
+				//Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s", i, rules[i].regex, position, substr_len, substr_len, substr_start);
 				position += substr_len;
 
 				/* TODO: Now a new token is recognized with rules[i]. Add codes

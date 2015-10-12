10985399ad6199b5c642f49a7a4f49ea8be61f9f
tracer-ics2015 <tracer@njuics.org>
2015-10-12 20:04:19 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:04:19 up  3:15,  2 users,  load average: 0.16, 0.12, 0.07 8048d0964dd6b7cc437b6c582f967e94fd54d83

--- nemu/src/cpu/exec/misc/jcc-template.h
+++ nemu/src/cpu/exec/misc/jcc-template.h
@@ -6,7 +6,6 @@ static void do_jcc_execute(bool flag,int diplacement,char jcc_instr[])
 	{
 		DATA_TYPE_S eip_dis = op_src->val;
 		cpu.eip = cpu.eip + eip_dis;
-		printf("%s $0x%x",jcc_instr,cpu.eip+diplacement);
 	}
 	print_asm("%s $0x%x",jcc_instr,cpu.eip+diplacement);
 }

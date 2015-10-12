cae923127c433f29a845e75d9eb48e930d66a7c5
tracer-ics2015 <tracer@njuics.org>
2015-10-12 21:31:28 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:31:28 up  4:42,  2 users,  load average: 0.17, 0.09, 0.06 117b4c6af76b039d178e612680cf9176eb83b773

--- nemu/src/cpu/exec/arith/sub-template.h
+++ nemu/src/cpu/exec/arith/sub-template.h
@@ -3,12 +3,13 @@
 #define instr sub
 
 static void do_execute() {
-	OPERAND_W(op_dest, op_dest->val-op_src->val);
+	int dst = op_dest->val - op_src->val;
 	cpu.OF = !subOK(op_dest->val, op_src->val);
-	cpu.SF = (op_dest->val)>>31;
-	cpu.ZF = (op_dest->val == 0);
-	cpu.PF = anyEvenBit(op_dest->val);
-	cpu.CF = (op_src->val < op_dest->val);
+	cpu.SF = (dst)>>31;
+	cpu.ZF = (dst == 0);
+	cpu.PF = anyEvenBit(dst);
+	cpu.CF = (op_dest->val < op_src->val);
+	OPERAND_W(op_dest, op_dest->val-op_src->val);
 	print_asm_template2();
 }
 

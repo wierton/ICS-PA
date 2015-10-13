39c7ce43df03d7d14db4bcd04568fe1e7d901224
tracer-ics2015 <tracer@njuics.org>
2015-10-13 12:43:36 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:43:36 up 24 min,  2 users,  load average: 0.18, 0.08, 0.06 4d07d4899d2020f7937317e4ded393a49c7e6ccb

--- nemu/include/cpu/exec/helper.h
+++ nemu/include/cpu/exec/helper.h
@@ -20,8 +20,9 @@
 
 #define make_eflags_helper(type, flag1, flag2)\
 	make_helper(concat5(flagstr, _, type, _, SUFFIX)){ \
-	print_asm(str(flagstr)" %s",op_src->str);\
+	printf("$1:"str(flagstr)" %s\n",op_src->str);\
 	int len=concat4(decode_,type,_, SUFFIX)(cpu.eip+1);\
+	printf("$2:"str(flagstr)" %s\n\n",op_src->str);\
 	bool flag = ((flag1) && (flag2));\
 	do_eflags_execute(flag,len+1,str(flagstr));\
 	return len+1;\

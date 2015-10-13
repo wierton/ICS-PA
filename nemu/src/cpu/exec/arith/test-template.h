8c1587c1d68638d048edc97be7e63ae38175e5e1
tracer-ics2015 <tracer@njuics.org>
2015-10-13 23:47:16 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:47:16 up  4:55,  2 users,  load average: 0.00, 0.02, 0.05 5fdf67675fefaf326e1fd76d44ade7565b53c588

--- nemu/src/cpu/exec/arith/test-template.h
+++ nemu/src/cpu/exec/arith/test-template.h
@@ -6,7 +6,7 @@
 static void do_execute() {
 	DATA_TYPE dst = op_src->val & op_dest->val;
 	cpu.OF = 0;
-	cpu.SF = dst>>31;
+	cpu.SF = MSB(dst);
 	cpu.ZF = (dst == 0);
 	cpu.PF = anyEvenBit(op_dest->val);
 	cpu.CF = 0;

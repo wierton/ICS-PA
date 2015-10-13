2b44fd06b9cfc321192ebf151bab82f05a7e3eae
tracer-ics2015 <tracer@njuics.org>
2015-10-13 12:40:33 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:40:33 up 21 min,  2 users,  load average: 0.01, 0.04, 0.05 109ab0b51890d58fd3bb9114fc0e2b7c54efd863

--- nemu/src/cpu/exec/data-mov/set-template.h
+++ nemu/src/cpu/exec/data-mov/set-template.h
@@ -14,123 +14,123 @@ static void do_eflags_execute(bool flag,int diplacement,char set_flagstr[])
 
 
 #define flagstr seta
-make_eflags_helper(rm_1,!cpu.CF,!cpu.ZF)
+make_eflags_helper(i2r,!cpu.CF,!cpu.ZF)
 #undef flagstr
 
 #define flagstr setae
-make_eflags_helper(rm_1,!cpu.CF,1)
+make_eflags_helper(i2r,!cpu.CF,1)
 #undef flagstr
 
 #define flagstr setb
-make_eflags_helper(rm_1,cpu.CF,1)
+make_eflags_helper(i2r,cpu.CF,1)
 #undef flagstr
 
 #define flagstr setbe
-make_eflags_helper(rm_1,cpu.CF || cpu.ZF,1)
+make_eflags_helper(i2r,cpu.CF || cpu.ZF,1)
 #undef flagstr
 
 #define flagstr setc
-make_eflags_helper(rm_1,cpu.CF,1)
+make_eflags_helper(i2r,cpu.CF,1)
 #undef flagstr
 
 #define flagstr sete
-make_eflags_helper(rm_1,cpu.ZF,1)
+make_eflags_helper(i2r,cpu.ZF,1)
 #undef flagstr
 
 #define flagstr setz
-make_eflags_helper(rm_1,cpu.ZF,1)
+make_eflags_helper(i2r,cpu.ZF,1)
 #undef flagstr
 
 #define flagstr setg
-make_eflags_helper(rm_1,!cpu.ZF,cpu.SF==cpu.OF)
+make_eflags_helper(i2r,!cpu.ZF,cpu.SF==cpu.OF)
 #undef flagstr
 
 #define flagstr setge
-make_eflags_helper(rm_1,cpu.SF==cpu.OF,1)
+make_eflags_helper(i2r,cpu.SF==cpu.OF,1)
 #undef flagstr
 
 #define flagstr setl
-make_eflags_helper(rm_1,cpu.SF!=cpu.OF,1)
+make_eflags_helper(i2r,cpu.SF!=cpu.OF,1)
 #undef flagstr
 
 #define flagstr setle
-make_eflags_helper(rm_1,(cpu.ZF || (cpu.SF!=cpu.OF)),1)
+make_eflags_helper(i2r,(cpu.ZF || (cpu.SF!=cpu.OF)),1)
 #undef flagstr
 
 #define flagstr setna
-make_eflags_helper(rm_1,cpu.CF,cpu.SF)
+make_eflags_helper(i2r,cpu.CF,cpu.SF)
 #undef flagstr
 
 #define flagstr setnae
-make_eflags_helper(rm_1,cpu.CF,1)
+make_eflags_helper(i2r,cpu.CF,1)
 #undef flagstr
 
 #define flagstr setnb
-make_eflags_helper(rm_1,!cpu.CF,1)
+make_eflags_helper(i2r,!cpu.CF,1)
 #undef flagstr
 
 #define flagstr setnbe
-make_eflags_helper(rm_1,!cpu.ZF,!cpu.ZF)
+make_eflags_helper(i2r,!cpu.ZF,!cpu.ZF)
 #undef flagstr
 
 #define flagstr setnc
-make_eflags_helper(rm_1,!cpu.CF,1)
+make_eflags_helper(i2r,!cpu.CF,1)
 #undef flagstr
 
 #define flagstr setne
-make_eflags_helper(rm_1,!cpu.ZF,1)
+make_eflags_helper(i2r,!cpu.ZF,1)
 #undef flagstr
 
 #define flagstr setng
-make_eflags_helper(rm_1,cpu.ZF || (cpu.SF!=cpu.OF),1)
+make_eflags_helper(i2r,cpu.ZF || (cpu.SF!=cpu.OF),1)
 #undef flagstr
 
 #define flagstr setnge
-make_eflags_helper(rm_1,cpu.SF!=cpu.OF,1)
+make_eflags_helper(i2r,cpu.SF!=cpu.OF,1)
 #undef flagstr
 
 #define flagstr setnl
-make_eflags_helper(rm_1,cpu.SF==cpu.OF,1)
+make_eflags_helper(i2r,cpu.SF==cpu.OF,1)
 #undef flagstr
 
 #define flagstr setnle
-make_eflags_helper(rm_1,!cpu.ZF,cpu.SF==cpu.OF)
+make_eflags_helper(i2r,!cpu.ZF,cpu.SF==cpu.OF)
 #undef flagstr
 
 #define flagstr setno
-make_eflags_helper(rm_1,!cpu.OF,1)
+make_eflags_helper(i2r,!cpu.OF,1)
 #undef flagstr
 
 #define flagstr setnp
-make_eflags_helper(rm_1,!cpu.PF,1)
+make_eflags_helper(i2r,!cpu.PF,1)
 #undef flagstr
 
 #define flagstr setns
-make_eflags_helper(rm_1,!cpu.SF,1)
+make_eflags_helper(i2r,!cpu.SF,1)
 #undef flagstr
 
 #define flagstr setnz
-make_eflags_helper(rm_1,!cpu.ZF,1)
+make_eflags_helper(i2r,!cpu.ZF,1)
 #undef flagstr
 
 #define flagstr seto
-make_eflags_helper(rm_1,cpu.OF,1)
+make_eflags_helper(i2r,cpu.OF,1)
 #undef flagstr
 
 #define flagstr setp
-make_eflags_helper(rm_1,cpu.PF,1)
+make_eflags_helper(i2r,cpu.PF,1)
 #undef flagstr
 
 #define flagstr setpe
-make_eflags_helper(rm_1,cpu.PF,1)
+make_eflags_helper(i2r,cpu.PF,1)
 #undef flagstr
 
 #define flagstr setpo
-make_eflags_helper(rm_1,!cpu.PF,1)
+make_eflags_helper(i2r,!cpu.PF,1)
 #undef flagstr
 
 #define flagstr sets
-make_eflags_helper(rm_1,cpu.SF,1)
+make_eflags_helper(i2r,cpu.SF,1)
 #undef flagstr
 
 

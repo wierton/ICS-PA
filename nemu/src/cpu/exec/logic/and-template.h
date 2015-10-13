868dfa3f3e1dbdafff8de8f1017df72b30426a0c
tracer-ics2015 <tracer@njuics.org>
2015-10-13 21:56:17 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:56:17 up  3:04,  2 users,  load average: 0.00, 0.01, 0.05 d1ff5b41898f92ab2f2b27f7d73636a398381e4

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -5,23 +5,58 @@
 static void do_execute () {
 	DATA_TYPE result = op_dest->val & op_src->val;
 	OPERAND_W(op_dest, result);
-	printf("and:%x,%x",op_dest->val, op_src->val);
 
 	/* TODO: Update EFLAGS. */
-	DATA_TYPE dst = op_dest->val & op_src->val;
 	cpu.OF = 0;
-	cpu.SF = (dst)>>31;
-	cpu.ZF = (dst == 0);
-	cpu.PF = anyEvenBit(dst);
+	cpu.SF = MSB(result);
+	cpu.ZF = (result == 0);
+	cpu.PF = anyEvenBit(result);
 	cpu.CF = 0;
 
 	print_asm_template2();
 }
 
-make_instr_helper(i2a)
-make_instr_helper(i2rm)
+#if DATA_BYTE == 1
+make_helper(and_i2a_b)
+{
+	int len = decode_i2a_b(eip + 1);
+	int src_val = op_src->val;
+	src_val = (((src_val)<<24)>>24);
+	DATA_TYPE result = op_dest->val & src_val;
+	printf("and_i2a_b:%x\n",src_val);
+	OPERAND_W(op_dest, result);
+	cpu.OF = 0;
+	cpu.SF = MSB(result);
+	cpu.ZF = (result == 0);
+	cpu.PF = anyEvenBit(result);
+	cpu.CF = 0;
+
+	print_asm_template2();
+	return len + 1;
+}
+
+make_helper(and_i2rm_b)
+{
+	int len = decode_i2rm_b(eip + 1);
+	int src_val = op_src->val;
+	src_val = (((src_val)<<24)>>24);
+	DATA_TYPE result = op_dest->val & src_val;
+	printf("and_i2rm_b:%x\n",src_val);
+	OPERAND_W(op_dest, result);
+	cpu.OF = 0;
+	cpu.SF = MSB(result);
+	cpu.ZF = (result == 0);
+	cpu.PF = anyEvenBit(result);
+	cpu.CF = 0;
+
+	print_asm_template2();
+	return len + 1;
+}
+#endif
 
 #if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
 make_instr_helper(si2rm)
 #endif
 

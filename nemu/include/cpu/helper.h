3164a00c84603a12950552b2490812b34b31bcc3
tracer-ics2015 <tracer@njuics.org>
2015-10-13 16:57:58 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:57:58 up 52 min,  2 users,  load average: 0.16, 0.05, 0.06 7d3873df919caad9b16be18ab51902c4bb9d48de

--- nemu/include/cpu/helper.h
+++ nemu/include/cpu/helper.h
@@ -8,6 +8,7 @@
 #define make_helper(name) int name(swaddr_t eip)
 
 static inline uint32_t instr_fetch(swaddr_t addr, size_t len) {
+	printf("instr_fetch:$0x%x\n",cpu.eip);
 	return swaddr_read(addr, len);
 }
 

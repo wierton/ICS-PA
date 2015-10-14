98cdec1dd8554573e0f4bb180ba2ab038a429c0f
tracer-ics2015 <tracer@njuics.org>
2015-10-14 21:20:04 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:20:04 up  4:45,  2 users,  load average: 0.01, 0.06, 0.05 971e69f64972de3b6b332e6c3ee6bae10992f22

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -5,6 +5,7 @@
 static void do_execute() {
 	DATA_TYPE dst_2 = op_dest->val + op_src->val;
 	DATA_TYPE dst_3 = op_dest->val + op_src->val + cpu.CF;
+	OPERAND_W(op_dest, op_dest->val + op_src->val +cpu.CF);
 	int o2f = !addOK(op_dest->val, op_src->val);
 	if (o2f || (dst_3 < dst_2))
 		cpu.OF = 1;
@@ -14,8 +15,6 @@ static void do_execute() {
 	cpu.ZF = (dst_3 == 0);
 	cpu.PF = anyEvenBit(dst_3);
 	cpu.CF = cpu.OF;
-	printf("%d %d %d %d\n",op_dest->val,op_src->val,cpu.CF,op_dest->val + op_src->val + cpu.CF);
-	OPERAND_W(op_dest, op_dest->val + op_src->val + cpu.CF);
 	print_asm_template2();
 }
 

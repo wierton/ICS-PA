9c01f3760b0466c35c2597c04e050631b77e2451
tracer-ics2015 <tracer@njuics.org>
2015-10-14 14:54:48 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:54:49 up 50 min,  2 users,  load average: 0.15, 0.06, 0.06 faa5d0f65a5b81a96969b4af2b27c3ba5449a79

--- nemu/src/cpu/exec/arith/adc-template.h
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -3,13 +3,18 @@
 #define instr adc
 
 static void do_execute() {
-	int dst = op_dest->val + op_src->val;
-	cpu.OF = !addOK(op_dest->val, op_src->val);
-	cpu.SF = (dst)>>31;
-	cpu.ZF = (dst == 0);
-	cpu.PF = anyEvenBit(dst);
+	int dst_2 = op_dest->val + op_src->val;
+	int dst_3 = op_dest->val + op_src->val + cpu.OF;
+	int o2f = !addOK(op_dest->val, op_src->val);
+	if (o2f || (dst_3 < dst_2))
+		cpu.OF = 1;
+	else
+		cpu.OF = 0;
+	cpu.SF = (dst_3)>>31;
+	cpu.ZF = (dst_3 == 0);
+	cpu.PF = anyEvenBit(dst_3);
 	cpu.CF = !addOK(op_dest->val, op_src->val);
-	OPERAND_W(op_dest, op_dest->val+op_src->val);
+	OPERAND_W(op_dest, op_dest->val + op_src->val + cpu.CF);
 	print_asm_template2();
 }
 

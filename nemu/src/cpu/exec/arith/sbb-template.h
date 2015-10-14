a8b5d06ef8641e65d350483923c5aeddf2976a69
tracer-ics2015 <tracer@njuics.org>
2015-10-14 14:57:43 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:57:43 up 53 min,  2 users,  load average: 0.01, 0.04, 0.05 88b5180c4f8a86a227e83cd17d5de43d9d8b855f

---
+++ nemu/src/cpu/exec/arith/sbb-template.h
@@ -0,0 +1,26 @@
+#include "cpu/exec/template-start.h"
+
+#define instr sbb
+
+static void do_execute() {
+	int dst = op_dest->val - op_src->val;
+	cpu.OF = !subOK(op_dest->val, op_src->val);
+	cpu.SF = (dst)>>31;
+	cpu.ZF = (dst == 0);
+	cpu.PF = anyEvenBit(dst);
+	cpu.CF = (op_dest->val < op_src->val);
+	OPERAND_W(op_dest, op_dest->val-op_src->val);
+	print_asm_template2();
+}
+
+make_instr_helper(i2a)
+make_instr_helper(i2rm)
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#if DATA_BYTE != 1
+make_instr_helper(si2rm)
+#endif
+
+#include "cpu/exec/template-end.h"
+

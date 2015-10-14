202f794398ad5c28eab02e6884ad1bcefae1c08c
tracer-ics2015 <tracer@njuics.org>
2015-10-14 19:40:03 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:40:03 up  3:05,  2 users,  load average: 0.04, 0.17, 0.12 ee59b3b119541790830009d668c067ada60d1bb5

--- nemu/src/cpu/exec/misc/jmp-template.h
+++ nemu/src/cpu/exec/misc/jmp-template.h
@@ -7,7 +7,15 @@ static void do_execute() {
 	print_asm_template1();
 }
 
-make_instr_helper(rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_helper(concat(jmp_rm_, SUFFIX))
+{
+	int len = concat(decode_rm_,SUFFIX)(eip+1);
+	cpu.eip = op_src->val;
+	return len+1;
+
+}
+#endif
 make_instr_helper(i)
 
 #include "cpu/exec/template-end.h"

f67acc3576d18c02a093788d6fc0d7820306c91e
tracer-ics2015 <tracer@njuics.org>
2015-10-15 19:44:51 +0800

    >  run 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:44:51 up  1:10,  2 users,  load average: 0.00, 0.01, 0.05 4b115d2815b1ec449b0c2a2a4ee433daabfa45

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -15,7 +15,6 @@ FLOAT f2F(float a) {
 	int frac=((uf&0x7fffff)|0x800000);
 //	int q=(uf&0x807fffff)|((j>>8)|0x800000);
 	int power=((uf&0x7f800000)>>23)-127-23;
-	frac = (sign>>8)|frac;
 	if(power>=-16)
 	{
 		if(sign)

5f7408387423a741f15954ff86ec8c9761d25636
tracer-ics2015 <tracer@njuics.org>
2015-10-15 12:28:43 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:28:43 up  2:12,  2 users,  load average: 0.03, 0.04, 0.05 6bc1ddf64a166a08a0d17f591f5a77489c305af

--- nemu/src/cpu/exec/misc/ret-template.h
+++ nemu/src/cpu/exec/misc/ret-template.h
@@ -2,6 +2,7 @@
 
 #define instr ret
 
+#if DATA_BYTE == 4
 make_helper(ret)
 {
 	cpu.eip = MEM_R(cpu.esp);
@@ -9,6 +10,15 @@ make_helper(ret)
 	print_asm("ret");
 	return 1;
 }
+#endif
 
+make_helper(concat(ret_,SUFFIX))
+{
+	cpu.eip = MEM_R(cpu.esp);
+	int len = concat(decode_i_,SUFFIX)(eip + 1);
+	cpu.esp += DATA_BYTE;
+	print_asm("ret");
+	return len + 1;
+}
 
 #include "cpu/exec/template-end.h"

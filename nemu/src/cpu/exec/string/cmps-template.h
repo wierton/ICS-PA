da7c4a6f8b7c9348a4c37a06715e52cdb0c8c5af
tracer-ics2015 <tracer@njuics.org>
2015-10-15 11:44:25 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:44:25 up  1:28,  2 users,  load average: 0.01, 0.02, 0.05 d7a2b47127a84b87db906d7c75fc0623eee6a3f2

---
+++ nemu/src/cpu/exec/string/cmps-template.h
@@ -0,0 +1,22 @@
+#include "cpu/exec/template-start.h"
+
+#define instr cmps
+
+make_helper(concat(cmps_,SUFFIX))
+{
+	DATA_TYPE dst = MEM_R(cpu.edi);
+	DATA_TYPE src = MEM_R(cpu.esi);
+	int32_t result = src - dst;
+	cpu.OF = !subOK(dst, src);
+	cpu.SF = MSB(result);
+	cpu.ZF = (result == 0);
+	cpu.PF = anyEvenBit(result);
+	cpu.CF = (dst < src);
+	cpu.edi += DATA_BYTE;
+	cpu.esi += DATA_BYTE;
+
+	print_asm("cmpsb $es:($edi),$ds:($esi)");
+	return 1;
+}
+
+#include "cpu/exec/template-end.h"

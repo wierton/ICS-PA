ffd0ce8f32e01bab3187e8c49124aebb883a9c92
tracer-ics2015 <tracer@njuics.org>
2015-10-16 13:19:40 +0800

    >  run 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:19:40 up 49 min,  2 users,  load average: 0.00, 0.01, 0.05 3c618caa2e92e3cb1ce4365e27b535e324942d62

--- testcase/src/quadratic-eq.c
+++ testcase/src/quadratic-eq.c
@@ -19,7 +19,7 @@ int main() {
 
 	FLOAT sqrt_dt = sqrt(dt);
 
-	nemu_assert((F_div_F(f2F(0.7),f2F(1.000)),f2F(0.7))<f2F(1e-4));
+	nemu_assert((F_div_F(f2F(0.7),f2F(1.000))-f2F(0.7))<f2F(1e-4));
 
 	FLOAT x1 = F_div_F(-b + sqrt_dt, F_mul_int(a, 2));
 	FLOAT x2 = F_div_F(-b - sqrt_dt, F_mul_int(a, 2));

fcc2cf86c2ce99b3fe80ad37ef25f3bf88f55471
tracer-ics2015 <tracer@njuics.org>
2015-10-17 11:05:37 +0800

    >  run 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:05:37 up  1:35,  2 users,  load average: 0.12, 0.04, 0.05 bdb1ab72ff56bad8868c37cc22e75b8f9ebe5067

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -83,14 +83,26 @@ unsigned div(unsigned a[],unsigned b[],unsigned c[])
 {
 	unsigned x=B(a[2],a[1]);
 	unsigned y=B(b[1],b[0]);
+	unsigned m[4];
+	c[2]=0;
 	if(b[1] != 0)
 	{
 		unsigned CF=x-y*(x/y),t=x/y;
 		c[1]=L(t);
 		a[2]=H(CF);
 		a[1]=L(CF);
-		c[0]=0;
-		while(sub(a,b)) c[0]++;
+		c[0]=B(a[2],a[1])/b[1];
+		mul(b,c,m);
+		if(cmp(a,m))
+		{
+			sub(a,m);
+			while(sub(a,b)) c[0]++;
+		}
+		else
+		{
+			sub(m,a);
+			while(sub(m,b)) c[0]--;
+		}
 	}
 	else
 	{
@@ -99,8 +111,18 @@ unsigned div(unsigned a[],unsigned b[],unsigned c[])
 		c[1]=L(t);
 		a[2]=H(CF);
 		a[1]=L(CF);
-		c[0]=0;
-		while(sub(a,b)) c[0]++;
+		c[0]=B(a[1],a[0])/b[0];
+		mul(b,c,m);
+		if(cmp(a,m))
+		{
+			sub(a,m);
+			while(sub(a,b)) c[0]++;
+		}
+		else
+		{
+			sub(m,a);
+			while(sub(m,b)) c[0]--;
+		}
 	}
 	return 1;
 }

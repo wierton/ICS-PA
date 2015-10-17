0071c759aed86310c47180c1c3a99cca9ed5a333
tracer-ics2015 <tracer@njuics.org>
2015-10-17 12:14:19 +0800

    >  run 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:14:19 up  2:44,  2 users,  load average: 0.41, 0.16, 0.08 69bc1282e516d4a1b9618e093a6e1cafe460c38e

--- lib-common/FLOAT.c
+++ lib-common/FLOAT.c
@@ -91,7 +91,7 @@ unsigned div(unsigned a[],unsigned b[],unsigned c[])
 		c[1]=L(t);
 		a[2]=H(CF);
 		a[1]=L(CF);
-		z[0]=L(((a[2]<<16)/b[1])+((a[1]*b[1]-a[2]*b[0])<<16)/(b[1]*B(b[1],b[0])));;
+		z[0]=B(a[2],a[1])/b[1];
 		mul(b,z,m);
 		if(cmp(a,m))
 		{

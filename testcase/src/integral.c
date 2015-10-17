2a890cd6496d84d92cb4b9f15dad5995df013d0d
tracer-ics2015 <tracer@njuics.org>
2015-10-17 10:07:31 +0800

    >  run 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:07:31 up 37 min,  2 users,  load average: 0.02, 0.04, 0.05 ca3311791cfa7f1edb4d0cfd7a205b5399ee8ce5

--- testcase/src/integral.c
+++ testcase/src/integral.c
@@ -19,7 +19,7 @@ FLOAT computeT(int n, FLOAT a, FLOAT b, FLOAT (*fun)(FLOAT)) {
 }
 
 int main() {
-	nemu_assert(Fabs(F_mul_F(f2F(3.4),f2F(4.2))-f2F(14.2799))<Fabs(1e-4));
+	nemu_assert(Fabs(F_mul_F(f2F(3.4),f2F(4.2))-f2F(14.2799))<f2F(1e-4));
 	FLOAT a = computeT(10, f2F(-1.0), f2F(1.0), f);
 	FLOAT ans = f2F(0.551222);
 

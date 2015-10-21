4e2841e8aec3321672bf8dcea3157f8a7ad8605e
tracer-ics2015 <tracer@njuics.org>
2015-10-21 21:40:00 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:40:00 up  2:58,  2 users,  load average: 0.11, 0.09, 0.13 a462c2ebba8e56a18acc11eeaf997795e058240

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -74,8 +74,7 @@ void cpu_exec(volatile uint32_t n) {
 #endif
 
 		/* TODO: check watchpoints here. */
-		if(!check_wp())
-			nemu_state = STOP;
+		if(!check_wp()) {nemu_state = STOP;}
 
 		if(nemu_state != RUNNING) { return; }
 	}

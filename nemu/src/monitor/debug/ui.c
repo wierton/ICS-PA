c3151b323a334afa8af95fd7b0b73ccc956e1a7f
tracer-ics2015 <tracer@njuics.org>
2015-10-21 19:58:47 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:58:47 up  1:17,  2 users,  load average: 0.00, 0.03, 0.05 dd3074a29f485c467b0f97046403993589c7403e

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -258,7 +258,7 @@ static int cmd_bt(char *args)
 			printf("No stack!\n");
 			return 0;
 		}
-		printf("#%d\t0x%08x in %s\n", no++, ret_addr + 1, func_name);
+		printf("#%d\t0x%08x in %s\n", no++, ret_addr, func_name);
 		prev_ebp = swaddr_read(now_ebp, 4);
 		ret_addr = swaddr_read(now_ebp + 4, 4);
 	}

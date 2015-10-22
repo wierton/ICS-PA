30d7460bc9822652717e13453e1eaf78c42409e0
tracer-ics2015 <tracer@njuics.org>
2015-10-22 13:00:49 +0800

    >  compile kernel 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  13:00:49 up  3:39,  2 users,  load average: 0.37, 0.15, 0.08 1fcfd4fc8a073aadddf56fcb4ddd5e30245922de

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -49,7 +49,7 @@ uint32_t loader() {
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			//memset((uint8_t *)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+			memset((uint8_t *)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE
 			/* Record the program break for future use. */

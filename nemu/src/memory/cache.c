e0a39682499b1069e6420a42c435db952c1dc002
tracer-ics2015 <tracer@njuics.org>
2015-11-18 14:10:42 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:10:42 up  1:24,  2 users,  load average: 0.00, 0.01, 0.05 32950528d337540a2efd4a6c1407dcc4d191751

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,8 +1,27 @@
 #include "common.h"
 
-#define NR_CCL 1024
+#define NR_CCL 64
+#define NR_RANK 1024
 
 typedef struct {
 	bool valid;
 	uint8_t buf[NR_CCL];
 } CCL;
+
+CCL cachebufs[NR_RANK];
+
+void init_cache() {
+	int i;
+	for(i = 0; i < NR_RANK; i ++)
+	{
+		cachebufs[i].valid = false;
+	}
+}
+
+/*
+static void cache_read(hwaddr_t addr, void *data) {
+}
+
+static void cache_write(hwaddr_t addr, void *data) {
+}
+*/

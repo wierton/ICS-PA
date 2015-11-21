21f731930cb0e78866b3032519803cfd1f3990af
tracer-ics2015 <tracer@njuics.org>
2015-11-21 21:45:00 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:45:00 up  4:06,  2 users,  load average: 0.06, 0.04, 0.05 624e00f83f7946d7dc8668f3c9887bd89928c037

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -14,7 +14,7 @@ uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
 }
 
 void hwaddr_write(hwaddr_t addr, size_t len, uint32_t data) {
-	printf("write indeed!\n");
+	printf("write indeed!\n\n");
 	cache_write(addr, len, data);
 }
 

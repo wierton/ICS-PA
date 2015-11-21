131645ec209d19fe7a0a628b48133b9f1e6270ed
tracer-ics2015 <tracer@njuics.org>
2015-11-21 22:52:30 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:52:30 up  5:13,  2 users,  load average: 0.00, 0.10, 0.21 73c119831f5b3fe9e964195be46236214abdbb4e

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,9 @@
 #include "common.h"
 
+/* define if necessary */
+#define DEBUG_MEMORY_CACHE
+
+
 uint32_t dram_read(hwaddr_t, size_t);
 void dram_write(hwaddr_t, size_t, uint32_t);
 
@@ -9,9 +13,9 @@ void cache_write(hwaddr_t, size_t, uint32_t);
 /* Memory accessing interfaces */
 
 uint32_t hwaddr_read(hwaddr_t addr, size_t len) {
-#ifdef DEBUG_CACHE
-	uint32_t cache_data = cache_read(addr, len);
-	uint32_t dram_data = dram_read(addr, len);
+#ifdef DEBUG_MEMORY_CACHE
+	uint32_t cache_data = cache_read(addr, len) & (~0u >> ((4 - len) << 3));
+	uint32_t dram_data = dram_read(addr, len) & (~0u >> ((4 - len) << 3));
 	if(cache_data != dram_data)
 		printf("data read error at 0x%x: (cache)0x%x\t(dram)0x%x\n", addr, cache_data, dram_data);
 #endif

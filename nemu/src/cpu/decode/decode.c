51de27045924932cf66c5d4cb1e4618b125e69ce
tracer-ics2015 <tracer@njuics.org>
2015-11-29 14:45:07 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:45:07 up  5:33,  2 users,  load average: 0.01, 0.02, 0.05 f268e66a79cfc2c4e0c475bddec32104c3ca993e

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -21,8 +21,23 @@ lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg)
 	assert(sreg >= 0 && sreg <6);
 	swaddr_t base = cpu.gsreg[sreg].TI?cpu.LDTR.base:cpu.GDTR.base;
 	SegDesc TargetSegDesc;
+	lnaddr_t ret_addr;
 	uint32_t *p = (uint32_t *)&TargetSegDesc;
 	*p = lnaddr_read(base, 4);
 	*(p+1) = lnaddr_read(base + 32, 4);
-	return 0;
+
+	uint32_t base_15_0 = TargetSegDesc.base_15_0;
+	uint32_t base_23_16 = TargetSegDesc.base_23_16;
+	uint32_t base_31_24 = TargetSegDesc.base_31_24;
+
+	/* Is the operation legal? */
+	assert(cpu.gsreg[sreg].RPL <= TargetSegDesc.privilege_level);
+
+	uint32_t sreg_base = ((base_15_0 | (base_23_16 << 16)) | (base_31_24 << 24));
+	/* judge if protect enable */
+	if(cpu.CR0.protect_enable)
+		ret_addr = addr + sreg_base;
+	else
+		ret_addr = addr;
+	return ret_addr;
 }

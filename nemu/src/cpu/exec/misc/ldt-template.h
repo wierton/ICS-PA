a19d3be404c6eda2a2b0380989f302a7fad54c11
tracer-ics2015 <tracer@njuics.org>
2015-11-29 23:46:15 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:46:15 up  1:34,  1 user,  load average: 0.07, 0.03, 0.05 a2fcb6909f8b8d88a5b5aba1dc62fb10923a2ed4

--- nemu/src/cpu/exec/misc/ldt-template.h
+++ nemu/src/cpu/exec/misc/ldt-template.h
@@ -1,4 +1,5 @@
 #include "cpu/exec/template-start.h"
+#include "cpu/decode/modrm.h"
 
 #if DATA_BYTE == 4
 
@@ -6,12 +7,26 @@
 
 make_helper(lgdt_l)
 {
-	int len = decode_rm_l(eip + 1);
-	printf("%x\n", op_src->val);
-	cpu.GDTR.limit = swaddr_read(op_src->val, 2, R_DS);
-	cpu.GDTR.base = swaddr_read(op_src->val + 2, 4, R_DS);
-	print_asm_template1();
-	return len + 1;
+	ModR_M m;
+	int ret;
+	swaddr_t addr;
+	m.val = instr_fetch(eip + 1, 1);
+	if(m.mod == 3)
+	{
+		addr = reg_l(m.R_M);
+		ret = 2;
+		print_asm("lgdtl %%%s", regsb[m.R_M]);
+	}
+	else
+	{
+		addr = swaddr_read(eip + 2, 4, R_DS);
+		ret = 6;
+		print_asm("lgdtl 0x%x", addr);
+	}
+	printf("%x\n", addr);
+	cpu.GDTR.limit = swaddr_read(addr, 2, R_DS);
+	cpu.GDTR.base = swaddr_read(addr + 2, 4, R_DS);
+	return ret;
 }
 
 #undef instr

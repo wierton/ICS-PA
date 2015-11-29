8d02bfa747b76c4a89bb87a053ea451676b0e060
tracer-ics2015 <tracer@njuics.org>
2015-11-29 14:48:42 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  14:48:42 up  5:36,  2 users,  load average: 0.00, 0.01, 0.05 9e1b932930908029c456b65f55e526d4de656c4b

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -67,15 +67,15 @@ void lnaddr_write(lnaddr_t addr, size_t len, uint32_t data) {
 	hwaddr_write(addr, len, data);
 }
 
-uint32_t swaddr_read(swaddr_t addr, size_t len) {
+uint32_t swaddr_read(swaddr_t addr, size_t len, uint8_t sreg) {
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif
-	lnaddr_t lnaddr = seg_translate(addr, len, 0);
+	lnaddr_t lnaddr = seg_translate(addr, len, sreg);
 	return lnaddr_read(lnaddr, len);
 }
 
-void swaddr_write(swaddr_t addr, size_t len, uint32_t data) {
+void swaddr_write(swaddr_t addr, size_t len, uint32_t data, uint8_t sreg) {
 #ifdef DEBUG
 	assert(len == 1 || len == 2 || len == 4);
 #endif

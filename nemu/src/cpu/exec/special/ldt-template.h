020491d3554195d2cb8a44a28b8f7d11d22bd490
tracer-ics2015 <tracer@njuics.org>
2015-11-30 10:34:31 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  10:34:31 up 23 min,  1 user,  load average: 0.00, 0.02, 0.05 8fec900e520c49709f8e1ea04124313d26344

--- nemu/src/cpu/exec/special/ldt-template.h
+++ nemu/src/cpu/exec/special/ldt-template.h
@@ -7,15 +7,26 @@
 
 make_helper(lgdt_l)
 {
-	int len;
+	ModR_M m;
+	int ret;
 	swaddr_t addr;
-	len = decode_i_rm2r_l(eip + 1);
-	addr = op_src->val;
-	printf("%x %x %x\n", op_src->val, op_dest->val,op_src2->val);
+	m.val = instr_fetch(eip + 1, 1);
+	if(m.mod == 3)
+	{
+		addr = reg_l(m.R_M);
+		ret = 2;
+		print_asm("lgdtl %%%s", regsb[m.R_M]);
+	}
+	else
+	{
+		addr = swaddr_read(eip + 2, 4, R_DS);
+		ret = 6;
+		print_asm("lgdtl 0x%x", addr);
+	}
+	
 	cpu.GDTR.limit = swaddr_read(addr, 2, R_DS);
 	cpu.GDTR.base = swaddr_read(addr + 2, 4, R_DS);
-	print_asm_template1();
-	return len + 1;
+	return ret;
 }
 
 #undef instr

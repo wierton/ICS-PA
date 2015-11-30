ca6335ac7418c79b3a9d5efd0823638c9455a881
tracer-ics2015 <tracer@njuics.org>
2015-11-30 09:27:05 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  09:27:05 up 44 min,  1 user,  load average: 0.09, 0.06, 0.05 66dbbfb6392de68359b97832d48174f7d7ce3c1

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -21,13 +21,15 @@ lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg)
 	assert(sreg >= 0 && sreg <6);
 	swaddr_t base = cpu.gsreg[sreg].TI?cpu.LDTR.base:cpu.GDTR.base;
 	SegDesc TargetSegDesc;
-	lnaddr_t ret_addr;
 	uint32_t sel = cpu.gsreg[sreg].INDEX;
 	uint32_t *p = (uint32_t *)&TargetSegDesc;
 
+	if(!cpu.CR0.protect_enable)
+		return addr;
+
 	printf("sel:0x%x lim:0x%x\n", sel, cpu.GDTR.limit);
 
-	assert(sel <= cpu.gsreg[sreg].TI?cpu.LDTR.limit:cpu.GDTR.limit);
+	assert(sel <= (cpu.gsreg[sreg].TI?cpu.LDTR.limit:cpu.GDTR.limit));
 	*p = lnaddr_read(base + 8*sel, 4);
 	*(p+1) = lnaddr_read(base + 4 + 8*sel, 4);
 
@@ -42,10 +44,6 @@ lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg)
 	assert(cpu.gsreg[sreg].RPL <= TargetSegDesc.privilege_level);
 
 	uint32_t sreg_base = ((base_15_0 | (base_23_16 << 16)) | (base_31_24 << 24));
-	/* judge if protect enable */
-	if(cpu.CR0.protect_enable)
-		ret_addr = addr + sreg_base;
-	else
-		ret_addr = addr;
-	return ret_addr;
+
+	return addr + sreg_base;
 }

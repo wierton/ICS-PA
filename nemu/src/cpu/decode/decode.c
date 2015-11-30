a1da7e59b15038ca459b481276901e10dde75d5a
tracer-ics2015 <tracer@njuics.org>
2015-11-30 09:22:05 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  09:22:05 up 39 min,  1 user,  load average: 0.02, 0.05, 0.05 7da53b25376947eadfeae3383bb0ada27fa8d08e

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -22,15 +22,20 @@ lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg)
 	swaddr_t base = cpu.gsreg[sreg].TI?cpu.LDTR.base:cpu.GDTR.base;
 	SegDesc TargetSegDesc;
 	lnaddr_t ret_addr;
+	uint32_t sel = cpu.gsreg[sreg].INDEX;
 	uint32_t *p = (uint32_t *)&TargetSegDesc;
-	*p = lnaddr_read(base, 4);
-	*(p+1) = lnaddr_read(base + 32, 4);
+
+	assert(sel <= cpu.gsreg[sreg].TI?cpu.LDTR.limit:cpu.GDTR.limit);
+	*p = lnaddr_read(base + 8*sel, 4);
+	*(p+1) = lnaddr_read(base + 4 + 8*sel, 4);
 
 	uint32_t base_15_0 = TargetSegDesc.base_15_0;
 	uint32_t base_23_16 = TargetSegDesc.base_23_16;
 	uint32_t base_31_24 = TargetSegDesc.base_31_24;
 
-	printf("base:%x\n",base);
+	/* judge if exceed the limit */
+	printf("base:%x\n", base);
+
 	/* Is the operation legal? */
 	assert(cpu.gsreg[sreg].RPL <= TargetSegDesc.privilege_level);
 

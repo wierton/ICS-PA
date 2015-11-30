4720f3270037baab7d63c75de1c67fe93102c706
tracer-ics2015 <tracer@njuics.org>
2015-11-30 11:07:22 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:07:22 up 55 min,  1 user,  load average: 0.01, 0.03, 0.05 1edae10bbcee38c9da345013f2d2c68ab59e8f

--- nemu/src/cpu/exec/transfer/jmp-template.h
+++ nemu/src/cpu/exec/transfer/jmp-template.h
@@ -23,11 +23,12 @@ make_instr_helper(i)
 #if DATA_BYTE == 4
 make_helper(jmp_ptr_l)
 {
-	cpu.eip = instr_fetch(eip + 1, 4);
+	int len = 7;
+	cpu.eip = instr_fetch(eip + 1, 4) - len;
 	printf("eip:%x\n", eip);
 	cpu.CS.val = instr_fetch(eip + 5, 2);
-	print_asm("jmp $0x%x,$0x%x", cpu.CS.val, cpu.eip);
-	return 7;
+	print_asm("ljmp $0x%x,$0x%x", cpu.CS.val, cpu.eip);
+	return len;
 }
 #endif
 

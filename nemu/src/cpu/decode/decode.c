319c9cae3dd0047abc3b32b72531128780dcd155
tracer-ics2015 <tracer@njuics.org>
2015-12-05 23:27:56 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:27:56 up  1:14,  1 user,  load average: 0.13, 0.06, 0.06 1469159ca44a2d3307fe891d762984279994b0d

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -24,7 +24,6 @@ lnaddr_t seg_translate(swaddr_t addr, size_t len, uint8_t sreg)
 	uint32_t sel = cpu.gsreg[sreg].INDEX;
 	uint32_t *p = (uint32_t *)&TargetSegDesc;
 
-	printf("sreg:%x\n", sreg);
 	if(!cpu.CR0.protect_enable || cpu.gsreg[sreg].val == 0x0)
 		return addr;
 

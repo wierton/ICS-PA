cbb9a7250aca0617d359166551aaf743bb83620f
tracer-ics2015 <tracer@njuics.org>
2015-12-06 11:19:46 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:19:46 up  1:48,  1 user,  load average: 0.00, 0.03, 0.07 b55cb0757f36e087febda5a1d43c9b6e856e7fb7

--- nemu/src/cpu/exec/string/stos-template.h
+++ nemu/src/cpu/exec/string/stos-template.h
@@ -6,7 +6,7 @@ make_helper(concat(stos_,SUFFIX))
 {
 	int IncDec = cpu.DF?-DATA_BYTE:DATA_BYTE;
 	DATA_TYPE result = cpu.eax;
-	MEM_W(cpu.edi,result, R_ES);
+	MEM_W(cpu.edi, result, R_ES);
 #if DATA_BYTE == 1
 	print_asm("stos $al,$es:($edi)");
 #endif
@@ -14,6 +14,7 @@ make_helper(concat(stos_,SUFFIX))
 	print_asm("stos $ax,$es:($edi)");
 #endif
 #if DATA_BYTE == 4
+	printf("%d\n", IncDec);
 	print_asm("stos $eax,$es:($edi)");
 #endif
 	cpu.edi += IncDec;

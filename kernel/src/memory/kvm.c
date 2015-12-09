6bba3dde8215f2fc3a4493bee919cc1cf0fccccb
tracer-ics2015 <tracer@njuics.org>
2015-12-09 23:41:31 +0800

    >  compile kernel 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:41:31 up  3:36,  1 user,  load average: 0.05, 0.04, 0.05 83d0dd0dada2a64d278eef5f1cd3d2b3e2aec769

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -12,7 +12,9 @@ inline PDE* get_kpdir() { return kpdir; }
 /* function for print info */
 #define print(...)\
 	const uint32_t *print_addr = (uint32_t *)(0x100000 - 200);\
-	sprintf((char *)print_addr, __VA_ARGS__);\
+	char print_str[200];\
+	sprintf(print_addr, __VA_ARGS__);\
+	memcpy((void *)print_addr, (void *)print_str, 199);\
 	asm volatile("bsf %eax,%eax;");
 
 /* set up page tables for kernel */
@@ -23,7 +25,6 @@ void init_page(void) {
 	PTE *ptable = (PTE *)va_to_pa(kptable);
 	uint32_t pdir_idx;
 	
-	//print("\n");
 	/* make all PDEs invalid */
 	memset(pdir, 0, NR_PDE * sizeof(PDE));
 

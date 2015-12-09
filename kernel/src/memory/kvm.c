6edb350346f4f4263ca67c354051b0a56583439c
tracer-ics2015 <tracer@njuics.org>
2015-12-09 23:47:33 +0800

    >  compile kernel 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  23:47:33 up  3:42,  1 user,  load average: 0.00, 0.01, 0.05 28f6b31447b5829efe11fad1d13cb08777bb9309

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -72,6 +72,7 @@ void init_page(void) {
 	cr0.val = read_cr0();
 	cr0.paging = 1;
 	write_cr0(cr0.val);
+	nemu_assert(0);
 }
 
 /* GDT in the kernel's memory, whose virtual memory is greater than 0xC0000000. */

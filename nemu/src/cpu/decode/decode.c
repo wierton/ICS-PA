c6ecb23960764fd5e12d06d0f6753bbeeae99e05
tracer-ics2015 <tracer@njuics.org>
2015-12-10 15:18:51 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  15:18:51 up  6:38,  1 user,  load average: 0.09, 0.04, 0.05 3b08f03ec85b5cb7f01f0d108193fb97dbe24c78

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -69,6 +69,7 @@ hwaddr_t page_translate(lnaddr_t addr)
 	{
 		ExecLog();
 		printf("addr:0x%x\n", addr);
+		printf("pagedir:0x%x\n", pageaddr.pagedir);
 		printf("paddr:0x%x\n", (cpu.CR3.page_directory_base << 12) + pageaddr.pagedir * 4);
 		printf("pdir:0x%x\n", pdir.val);
 	}

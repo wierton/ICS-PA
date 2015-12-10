0d243bdb77bc2c06cc21be0308bc16a90e3df90f
tracer-ics2015 <tracer@njuics.org>
2015-12-10 09:42:05 +0800

    >  compile kernel 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  09:42:05 up  1:01,  1 user,  load average: 0.07, 0.04, 0.05 fe9a341fbaa8d8383975d185a02bd24916e3b002

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -25,7 +25,7 @@ void init_page(void) {
 	PTE *ptable = (PTE *)va_to_pa(kptable);
 	uint32_t pdir_idx;
 
-	nemu_assert(NR_PTE >= 0x1000);	
+	nemu_assert(NR_PTE >= 0x500);	
 	/* make all PDEs invalid */
 	memset(pdir, 0, NR_PDE * sizeof(PDE));
 

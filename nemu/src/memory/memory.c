e48d8cff0f75600815fda071d4b4d0de33420fc0
tracer-ics2015 <tracer@njuics.org>
2015-12-10 12:41:24 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  12:41:24 up  4:00,  1 user,  load average: 0.01, 0.05, 0.05 2532727aaca755de89adf2b64f1310ad64ddbb07

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -83,7 +83,7 @@ uint32_t lnaddr_read(lnaddr_t addr, size_t len) {
 	assert(len == 1 || len == 2 || len == 4);
 	uint32_t addr_0_11 = addr & 0xfff;
 	hwaddr_t hwaddr = page_translate(addr);
-	if(addr_0_11 + len >= 0x1000)
+	if(addr_0_11 + len > 0x1000)
 	{
 		/* data cross the page boundary */
 		uint32_t ret;

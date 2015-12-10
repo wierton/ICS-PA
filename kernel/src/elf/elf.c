10d43b66e4020da5ce336bff7bee7014f6f61a24
tracer-ics2015 <tracer@njuics.org>
2015-12-10 17:41:44 +0800

    >  compile kernel 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:41:44 up  1:31,  1 user,  load average: 0.03, 0.05, 0.05 47045e209db2c29b8b8a1e8d4f874fbce7b83154

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -48,15 +48,14 @@ uint32_t loader() {
 			/* TODO: read the content of the segment from the ELF file 
 			 * to the memory region [VirtAddr, VirtAddr + FileSiz)
 			 */
-			prints("asdsafasdf");
+			prints("ph->p_vaddr");
+			printx((uint32_t)ph->p_vaddr);
 			printx((uint32_t)(va_to_pa(ph->p_vaddr)));
 			ramdisk_read((uint8_t *)va_to_pa(ph->p_vaddr), ph->p_offset, ph->p_filesz); 
 			/* TODO: zero the memory region 
 			 * [VirtAddr + FileSiz, VirtAddr + MemSiz)
 			 */
-			prints("asdsafasdf");
-			printx((uint32_t)(va_to_pa(ph->p_vaddr) + ph->p_filesz));
-
+			
 			memset((uint8_t *)(va_to_pa(ph->p_vaddr) + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 
 #ifdef IA32_PAGE

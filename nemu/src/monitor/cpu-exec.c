b6a83eedd982c52755556d1b73e00c02035a9ea8
tracer-ics2015 <tracer@njuics.org>
2015-12-12 20:28:03 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  20:28:03 up  1:53,  1 user,  load average: 0.06, 0.07, 0.18 ceb6c755319fdf5089f94e3793772fc783a4f268

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -71,7 +71,7 @@ void raise_intr(uint8_t no)
 	/* get the entry addr */
 	uint32_t EntryAddr = base + ((gd.offset_31_16 << 16) | gd.offset_15_0);
 
-	cpu.eip = EntryAddr + 1;
+	cpu.eip = EntryAddr - 2;
 
 	/* Jump back to cpu_exec() */
 	longjmp(jbuf, 1);

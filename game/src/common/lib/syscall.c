ad80c0915ed709855d8e92267d3f7d70af1d4c84
tracer-ics2015 <tracer@njuics.org>
2015-12-20 22:49:59 +0800

    >  compile game 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  22:49:59 up  5:56,  1 user,  load average: 0.16, 0.17, 0.31 9ccdbe5c56a8fa2b97f07dec7b7362ec86b3b36a

--- game/src/common/lib/syscall.c
+++ game/src/common/lib/syscall.c
@@ -3,25 +3,23 @@
 #include <sys/stat.h>
 
 int __attribute__((__noinline__))
-syscall(int id, ...) {
-	int ret;
-	int *args = &id;
-	asm volatile("int $0x80": "=a"(ret) : "a"(args[0]), "b"(args[1]), "c"(args[2]), "d"(args[3]));
-	return ret;
-}
+	syscall(int id, ...) {
+		int ret;
+		int *args = &id;
+		asm volatile("int $0x80": "=a"(ret) : "a"(args[0]), "b"(args[1]), "c"(args[2]), "d"(args[3]));
+		return ret;
+	}
 
 void _exit(int status) {
 	nemu_assert(!status);
 }
 
 int open(const char *pathname, int flags) {
-	nemu_assert(0);
-	return 0; 
+	return syscall(SYS_open, pathname, flags);
 }
 
 int read(int fd, char *buf, int len) {
-	nemu_assert(0);
-	return 0; 
+	return syscall(SYS_read, fd, buf, len); 
 }
 
 int write(int fd, char *buf, int len) {
@@ -29,8 +27,7 @@ int write(int fd, char *buf, int len) {
 }
 
 off_t lseek(int fd, off_t offset, int whence) {
-	nemu_assert(0);
-	return 0; 
+	return syscall(SYS_lseek, fd, offset, whence); 
 }
 
 void *sbrk(int incr) {
@@ -49,7 +46,7 @@ void *sbrk(int incr) {
 }
 
 int close(int fd) {
-	nemu_assert(0);
+	syscall(SYS_close, fd);
 	return 0; 
 }
 
--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -12,25 +12,56 @@ static const int keycode_array[] = {
 	K_s, K_f, K_p
 };
 
+/* K_UP,     K_DOWN,   K_LEFT,   K_RIGHT,  K_ESCAPE */
+/* 0x48/0xc8 0xd0/0x50 0x4b/0xcb 0x4d/0xcd 0x1/0x81 */
+/* K_RETURN, K_SPACE,  K_PAGEUP, K_PAGEDOWN, K_r */
+/* 0x1c/0x9c 0x39/0xb9 0x49/0xc9 0x51/0xd1   0x13/0x93*/
+/* K_a,      K_d,      K_e,      K_w,      K_q */
+/* 0x1e/0x9e 0x20/0xa0 0x12/0x92 0x11/0x91 0x90/0x10*/
+/* K_s,      K_f,      K_p */
+/* 0x1f/0x9f 0x21/0xa1 0x19/0x99 */
 static int key_state[NR_KEYS];
 
 void
 keyboard_event(void) {
 	/* TODO: Fetch the scancode and update the key states. */
-//	int i,targetkey;
-	uint8_t scancode = pio_read(0x60, 1);
-	Log("scancode:0x%x\n", scancode);
-/*	uint8_t updown = ((scancode >> 0x7) & 0x1);
+
+	int i,target_key;
+	uint32_t scancode = in_byte(0x60);
+	uint32_t updown = ((scancode >> 0x7) & 0x1);
 	for(i=0;i<NR_KEYS;i++)
 	{
-		if(keycode_array[i] == KEY_STATE_PRESS)
-			keycode_array[i] == KEY_STATE_WAIT_RELEASE;
+		if(key_state[i] == KEY_STATE_PRESS)
+			key_state[i] = KEY_STATE_WAIT_RELEASE;
+		if(key_state[i] == KEY_STATE_RELEASE)
+			key_state[i] = KEY_STATE_EMPTY;
 	}
 	switch(scancode & 0x7f)
 	{
-		case 
+		case 0x48:target_key = K_UP;break;
+		case 0xd0:target_key = K_DOWN;break;
+		case 0x4b:target_key = K_LEFT;break;
+		case 0x4d:target_key = K_RIGHT;break;
+		case 0x01:target_key = K_ESCAPE;break;
+		case 0x1c:target_key = K_RETURN;break;
+		case 0x39:target_key = K_SPACE;break;
+		case 0x49:target_key = K_PAGEUP;break;
+		case 0x51:target_key = K_PAGEDOWN;break;
+		case 0x13:target_key = K_r;break;
+		case 0x1e:target_key = K_a;break;
+		case 0x20:target_key = K_d;break;
+		case 0x12:target_key = K_e;break;
+		case 0x11:target_key = K_w;break;
+		case 0x90:target_key = K_q;break;
+		case 0x1f:target_key = K_s;break;
+		case 0x21:target_key = K_f;break;
+		case 0x19:target_key = K_p;break;
+		default:return;
 	}
-*/	assert(0);
+	if(updown)
+		key_state[target_key] = KEY_STATE_RELEASE;
+	else
+		key_state[target_key] = KEY_STATE_PRESS;
 }
 
 static inline int
@@ -68,7 +99,21 @@ process_keys(void (*key_press_callback)(int), void (*key_release_callback)(int))
 	 * Remember to enable interrupts before returning from the function.
 	 */
 
-	assert(0);
+	int i;
+	for(i=0;i<NR_KEYS;i++)
+	{
+		if(key_state[i] == KEY_STATE_PRESS)
+		{
+			key_press_callback(i);
+			return true;
+		}
+		if(key_state[i] == KEY_STATE_RELEASE)
+		{
+			key_release_callback(i);
+			return true;
+		}
+	}
+	
 	sti();
 	return false;
 }

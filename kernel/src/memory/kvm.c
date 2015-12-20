5d1a8427722d547b0ab1015813d4052c7bac4a9e
tracer-ics2015 <tracer@njuics.org>
2015-12-20 17:12:27 +0800

    >  compile kernel 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  17:12:27 up 18 min,  1 user,  load average: 0.08, 0.05, 0.08 77f0072effe113ee812db50e53a023c5be381830

--- kernel/src/memory/kvm.c
+++ kernel/src/memory/kvm.c
@@ -22,6 +22,7 @@ void init_page(void) {
 	/* make all PDEs invalid */
 	memset(pdir, 0, NR_PDE * sizeof(PDE));
 
+	printx(PHY_MEM / PT_SIZE);
 	/* fill PDEs */
 	for (pdir_idx = 0; pdir_idx < PHY_MEM / PT_SIZE; pdir_idx ++) {
 		pdir[pdir_idx].val = make_pde(ptable);

b06c24df7f432fec0fe2dd690a73d52b9b9acc16
tracer-ics2015 <tracer@njuics.org>
2015-12-31 16:44:27 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  16:44:27 up  1:07,  1 user,  load average: 0.00, 0.01, 0.06 4c06c36f694aacdfd8d7a97379b76a228a24e9d8

--- nemu/src/cpu/exec/transfer/ret.c
+++ nemu/src/cpu/exec/transfer/ret.c
@@ -5,24 +5,17 @@
 #undef DATA_BYTE
 
 /* for instruction encoding overloading */
-void stop_nemu();
+
 make_helper(iret)
 {
-	stop_nemu();
 	const uint32_t data_byte = 4;
-
-	printf("iret 0x%x\n", cpu.eip);
 	cpu.eip = swaddr_read(cpu.esp, data_byte, R_SS);
-	printf("iret 0x%x\n", cpu.eip);
 	cpu.esp += data_byte;
 	cpu.CS.val = swaddr_read(cpu.esp, data_byte, R_SS);
 	cpu.esp += data_byte;
 	cpu.EFLAGS = swaddr_read(cpu.esp, data_byte, R_SS);
 	cpu.esp += data_byte;
 
-	print_asm("iret 0x%x", cpu.eip);
-	printf("iret 0x%x\n", cpu.eip);
-	stop_nemu();
 	cpu.eip -= 1;
 	cpu.IF = 1;
 	return 1;

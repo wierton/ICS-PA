5c6abe83f0f1293eaa0d0563c133e4c062272f9e
tracer-ics2015 <tracer@njuics.org>
2016-01-07 00:16:35 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:16:35 up 8 min,  1 user,  load average: 0.08, 0.05, 0.05 6f10cfca99fb875acbd7fb9c7f12d1a89c01ff42

--- nemu/src/device/keyboard.c
+++ nemu/src/device/keyboard.c
@@ -9,11 +9,23 @@ static uint8_t *i8042_data_port_base;
 static bool newkey;
 
 void keyboard_intr(uint8_t scancode) {
-	if(nemu_state == RUNNING && newkey == false) {
+	static uint8_t old_key = 0xff;
+	static int calc_time = 0;
+
+	if(old_key != scancode)
+		calc_time = 0;
+	else
+		calc_time ++;
+
+	if(nemu_state == RUNNING && newkey == false &&calc_time == 20) 
+	{
 		i8042_data_port_base[0] = scancode;
 		i8259_raise_intr(KEYBOARD_IRQ);
+		calc_time = 0;
 /*		newkey = true;*/
 	}
+
+	old_key = scancode;
 }
 
 void i8042_io_handler(ioaddr_t addr, size_t len, bool is_write) {

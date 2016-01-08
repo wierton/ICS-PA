9a87c25a8b6ae10e78b29dbe97b9f9b2ab656173
tracer-ics2015 <tracer@njuics.org>
2016-01-09 01:49:17 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  01:49:17 up  4:17,  1 user,  load average: 0.35, 0.47, 0.45 5a5b509434aed21155f43dfe66c46d46cb05df26

--- nemu/src/device/sdl.c
+++ nemu/src/device/sdl.c
@@ -48,8 +48,8 @@ static void device_update(int signum) {
 		}
 	}
 
-//	int ret = setitimer(ITIMER_VIRTUAL, &it, NULL);
-//	Assert(ret == 0, "Can not set timer");
+	int ret = setitimer(ITIMER_VIRTUAL, &it, NULL);
+	Assert(ret == 0, "Can not set timer");
 }
 
 void sdl_clear_event_queue() {
@@ -84,8 +84,8 @@ void init_sdl() {
 
 	it.it_value.tv_sec = 0;
 	it.it_value.tv_usec = 1000000 / TIMER_HZ;
-	it.it_interval.tv_sec = 0;
-	it.it_interval.tv_usec = 1000000 / TIMER_HZ;
+//	it.it_interval.tv_sec = 0;
+//	it.it_interval.tv_usec = 1000000 / TIMER_HZ;
 	ret = setitimer(ITIMER_VIRTUAL, &it, NULL);
 	Assert(ret == 0, "Can not set timer");
 }

c71e6d6341b7afd2e05be63e197499df689e4b5f
tracer-ics2015 <tracer@njuics.org>
2016-01-08 21:41:19 +0800

    >  compile kernel 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  21:41:19 up 9 min,  1 user,  load average: 0.09, 0.26, 0.15 8bc393725e663c46ee82eac3adf8e5d9e01ccf0b

--- kernel/src/irq/irq_handle.c
+++ kernel/src/irq/irq_handle.c
@@ -50,6 +50,7 @@ void irq_handle(TrapFrame *tf) {
 	} else if (irq < 1000) {
 		panic("Unexpected exception #%d at eip = %x", irq, tf->eip);
 	} else if (irq >= 1000) {
+		nemu_assert(0);
 		int irq_id = irq - 1000;
 		assert(irq_id < NR_HARD_INTR);
 		struct IRQ_t *f = handles[irq_id];

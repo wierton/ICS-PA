faf4b6f76e31ab69cf8016ff5251dfd1baab5915
tracer-ics2015 <tracer@njuics.org>
2016-01-09 00:02:13 +0800

    >  compile game 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  00:02:13 up  2:30,  1 user,  load average: 0.14, 0.11, 0.15 54b2aacaef6f2e4caed49d8ae4fca79cdca3d5e6

--- game/src/nemu-pal/hal/video.c
+++ game/src/nemu-pal/hal/video.c
@@ -19,7 +19,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect,
 	 * is saved in ``dstrect'' after all clipping is performed
 	 * (``srcrect'' is not modified).
 	 */
-
+	Log("[");
 	int i,j;
 	int SrcX, SrcY, CopyWidth, CopyHeight, DstX, DstY;
 	if(srcrect == NULL || dstrect == NULL)
@@ -60,7 +60,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect,
 		SrcPos = SrcPos - CopyWidth + src->w;
 		DstPos = DstPos - CopyWidth + dst->w;
 	}
-
+	Log("]\n");
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {

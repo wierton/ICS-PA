2b587cf6ce4b3d1a485c457cf2325f60c34cbe69
tracer-ics2015 <tracer@njuics.org>
2016-01-16 01:34:15 +0800

    >  compile game 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  01:34:15 up 12:14,  1 user,  load average: 0.57, 0.53, 0.54 aadbb3b264657b56cdfbc24d85037c86b3bb70cd

--- game/src/nemu-pal/main.c
+++ game/src/nemu-pal/main.c
@@ -577,8 +577,8 @@ main_loop() {
    //
    // Show the trademark screen and splash screen
    //
-   //PAL_TrademarkScreen();
-   //PAL_SplashScreen();
+   PAL_TrademarkScreen();
+   PAL_SplashScreen();
 
    //
    // Run the main game routine

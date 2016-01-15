2b74fab690eb513adea2aa1f3faa2a601d929d8e
tracer-ics2015 <tracer@njuics.org>
2016-01-15 19:06:23 +0800

    >  compile game 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  19:06:23 up  5:47,  1 user,  load average: 0.02, 0.04, 0.05 a674cd959fce9bde9bd1fcdc6959daa703be2a3

--- game/src/common/lib/syscall.c
+++ game/src/common/lib/syscall.c
@@ -15,8 +15,7 @@ void _exit(int status) {
 }
 
 int open(const char *pathname, int flags) {
-	syscall(SYS_open, pathname, flags);
-	return 0;
+	return syscall(SYS_open, pathname, flags);
 }
 
 int read(int fd, char *buf, int len) {

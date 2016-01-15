6eed989f3029b419c89f0ee3a9425ac68ebefdab
tracer-ics2015 <tracer@njuics.org>
2016-01-16 01:10:52 +0800

    >  compile game 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  01:10:52 up 11:51,  1 user,  load average: 0.10, 0.60, 0.67 9f0e5bd4911f3d99ce1e38ef4c761fc31aefd206

--- game/src/nemu-pal/hal/keyboard.c
+++ game/src/nemu-pal/hal/keyboard.c
@@ -80,14 +80,14 @@ process_keys(void (*key_press_callback)(int), void (*key_release_callback)(int))
 	bool ret = false;
 	for(i=0;i<NR_KEYS;i++)
 	{
-		if(key_state[i] == KEY_STATE_PRESS && !ret)
-		{
-			key_press_callback(keycode_array[i]);
-			ret = true;
-		}
 		if(key_state[i] == KEY_STATE_RELEASE && !ret)
 		{
 			key_release_callback(keycode_array[i]);
+			ret = true;
+		}
+		if(key_state[i] == KEY_STATE_PRESS && !ret)
+		{
+			key_press_callback(keycode_array[i]);
 			ret = true;
 		}
 		clear_key(i);

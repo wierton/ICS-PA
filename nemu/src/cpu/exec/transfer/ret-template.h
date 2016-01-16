e282c0bf87487ebdef4b10dea5426f7139fb2c67
tracer-ics2015 <tracer@njuics.org>
2016-01-16 11:14:39 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:14:39 up  1:03,  1 user,  load average: 0.38, 0.80, 0.76 f8baa61e1da0c838b91e4ada14c8e7adad2f2b0

--- nemu/src/cpu/exec/transfer/ret-template.h
+++ nemu/src/cpu/exec/transfer/ret-template.h
@@ -10,7 +10,7 @@ make_helper(ret)
 	cpu.eip = MEM_R(cpu.esp, R_SS);
 	cpu.esp += DATA_BYTE;
 	print_asm("ret");
-	find_func_addr(cpu.eip);
+//	find_func_addr(cpu.eip);
 	return 1;
 }
 
@@ -22,7 +22,7 @@ make_helper(ret_w)
 	cpu.esp += DATA_BYTE;
 	cpu.esp += op_src->val;
 	print_asm("ret $0x%x",op_src->val);
-	find_func_addr(cpu.eip);
+//	find_func_addr(cpu.eip);
 	return len + 1;
 }
 
@@ -37,7 +37,7 @@ make_helper(iret)
 	cpu.esp += data_byte;
 
 	print_asm("iret 0x%x", cpu.eip);
-	find_func_addr(cpu.eip);
+//	find_func_addr(cpu.eip);
 	cpu.eip -= 1;
 	cpu.IF = 1;
 	return 1;

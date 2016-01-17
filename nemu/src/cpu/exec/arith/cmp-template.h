0ade84d681e2f4e63d5d10fa9782325d4289c6b5
tracer-ics2015 <tracer@njuics.org>
2016-01-17 11:03:52 +0800

    >  compile NEMU 141242068 nick Linux debian 3.16.0-4-586 #1 Debian 3.16.7-ckt11-1+deb8u3 (2015-08-04) i686 GNU/Linux  11:03:52 up 20 min,  1 user,  load average: 0.09, 0.12, 0.09 22b4925e6f9119228af718cf2ae563b658a77d05

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -11,7 +11,7 @@ static void do_execute() {
 	cpu.SF = MSB(result);
 	cpu.ZF = (result == 0);
 	cpu.PF = anyEvenBit(result);
-	cpu.CF = (dst < src);
+	cpu.CF = (op_dest->val < op_src->val);
 	print_asm_template2();
 }
 

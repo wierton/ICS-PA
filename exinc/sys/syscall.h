dcc968ca6a20d1c5dd7259c32fe05a7c57e7dcdc
141242068-ouxianfei <141242068@smail.nju.edu.cn>
2017-12-05 21:56:21 +0800

    change file mode from 755 to 644

--- exinc/sys/syscall.h
+++ exinc/sys/syscall.h
@@ -8,4 +8,11 @@
 #define SYS_lseek 19
 #define SYS_brk 45
 
+#define __NR_read	SYS_read
+#define __NR_write	SYS_write
+#define __NR_open	SYS_open
+#define __NR_close	SYS_close
+#define __NR_lseek	SYS_lseek
+#define __NR_brk	SYS_brk
+
 #endif
